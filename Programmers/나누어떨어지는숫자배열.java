ebdb6cd75b08bbda36372a83b394eda0a397224e
hyooosong <gythd1998@gmail.com>
2021-06-29 21:33:25 +0900

    [programmers] 나누어 떨어지는 숫자 배열

---
+++ Programmers/나누어떨어지는숫자배열.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i : arr) {
+            if(i % divisor == 0) {
+                answer.add(i);
+            }
+        }
+        
+        if(answer.size() == 0) {
+            answer.add(-1);
+        }
+        
+        Collections.sort(answer);
+        
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
