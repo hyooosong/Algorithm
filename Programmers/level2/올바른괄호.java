563dfb9cbd33d0c0216b15cfb5b4e539d56e31d2
hyooosong <gythd1998@gmail.com>
2021-07-29 23:30:51 +0900

    [programmers] 올바른 괄호

--- Programmers/level2/올바른괄호.java
+++ Programmers/level2/올바른괄호.java
@@ -1,5 +1,20 @@
-package level2;
+import java.util.Stack;
 
-public class 올바른괄호 {
-
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(!stack.empty()) {
+                if(s.charAt(i) == ')' && stack.peek() == '(')
+                    stack.pop();
+                else 
+                    stack.push(s.charAt(i));
+            } else {
+                stack.push(s.charAt(i));
+            }
+            
+        }
+        return stack.empty() ? true : false;
+    }
 }
